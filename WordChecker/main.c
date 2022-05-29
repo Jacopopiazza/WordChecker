53bafeb018394158f6ce0f1f14f92915feba27c2
JPiazza <jacopo@ideologica.it>
2022-05-29 23:38:55 +0200

    Initial Commit

---
+++ WordChecker/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  WordChecker
+//
+//  Created by Japo on 29/05/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
