1028a13f34db9cc77dd31df094a43dcf730598d0
Jacopopiazza <15248311+Jacopopiazza@users.noreply.github.com>
2022-05-29 23:57:32 +0200

    Added Libraries Folder and RBTree files

---
+++ WordChecker/Libraries/RBTree.c
@@ -0,0 +1,8 @@
+//
+//  RBTree.c
+//  WordChecker
+//
+//  Created by Japo on 29/05/22.
+//
+
+#include "RBTree.h"
---
+++ WordChecker/Libraries/RBTree.h
@@ -0,0 +1,13 @@
+//
+//  RBTree.h
+//  WordChecker
+//
+//  Created by Japo on 29/05/22.
+//
+
+#ifndef RBTree_h
+#define RBTree_h
+
+#include <stdio.h>
+
+#endif /* RBTree_h */
